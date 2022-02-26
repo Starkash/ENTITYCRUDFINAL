d3bb183eaddb2e8a18d23df6204a27442b0c96e2
Akash Kumar Sengar <akashsengar55@gmail.com>
2022-02-26 10:58:34 +0530

    Add project files.

---
+++ CRUDwithEntityFrameWork/Models/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDwithEntityFrameWork.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+
+        }
+
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ CRUDwithEntityFrameWork/Models/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDwithEntityFrameWork.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Display(Name="Employee Name")]
+        [Required(ErrorMessage ="Employee Name can't be Empty !")]
+        [MinLength(3,ErrorMessage ="Name must be minimum 3 char !")]
+        [MaxLength(30,ErrorMessage ="Name must be maximum 30 char !")]
+        public string Name { get; set; }
+
+        [Display(Name = "Employee Email Address")]
+        [Required(ErrorMessage = "Employee Email Address can't be Empty !")]
+        [EmailAddress(ErrorMessage ="Email address is not valid !")]
+        public string Email { get; set; }
+        
+        [Display(Name = "Employee Mobile Number")]
+        [Required(ErrorMessage = "Mobile Number can't be Empty !")]
+        [RegularExpression(@"^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$", ErrorMessage ="Invalid mobile number!")]
+        public string Mobile { get; set; }
+
+        [Display(Name = "Employee Address")]
+        [Required]
+        [MaxLength(200,ErrorMessage ="Max 200 char !")]
+        public string Address { get; set; }
+    }
+}
